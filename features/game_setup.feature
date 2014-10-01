d17fc028feb58699a4ad9a49e06424b9383c68f7
Bernard Mordan <bernardmordan@gmail.com>
2014-10-01 18:23:19 +0100

    basic board rendered for player one

--- features/game_setup.feature
+++ features/game_setup.feature
@@ -3,7 +3,7 @@ Feature: Setting up the Game!
  As a registered players
  I want to start the game
 
-Scenario: 
- Given I am player1
- When I follow "Start Game"
- Then I see my board
+  Scenario: 
+   Given I am player1
+   When I follow "Start Game"
+   Then I see my board
--- lib/main_game.rb
+++ lib/main_game.rb
@@ -1,6 +1,10 @@
 require 'sinatra/base'
 require_relative 'game'
 require_relative 'player'
+require_relative 'ship'
+require_relative 'board'
+require_relative 'cell'
+require_relative 'water'
 
 class Battleships < Sinatra::Base 
 	
@@ -8,7 +12,14 @@ class Battleships < Sinatra::Base
 	GAME = Game.new
   PLAYER1 = Player.new
   PLAYER2 = Player.new
-
+  FLEET1 = [Ship.aircraft_carrier, Ship.battleship, Ship.destroyer, Ship.submarine, Ship.patrol_boat ]
+  FLEET2 = [Ship.aircraft_carrier, Ship.battleship, Ship.destroyer, Ship.submarine, Ship.patrol_boat ]
+  BOARD1 = Board.new(Cell)
+  BOARD2 = Board.new(Cell)
+  FLEET1.each_with_index do |ship, index| 
+    coord = ("A" + (index + 1).to_s).to_sym
+    BOARD1.place(ship, coord, :vertically)
+  end
 
   set :views, Proc.new {File.join(root, '..', 'views')}
   set :public_folder, 'public'
@@ -21,18 +32,21 @@ class Battleships < Sinatra::Base
     @player1 = params[:player1]
     GAME.add_player(PLAYER1)
     GAME.player1.name = params[:player1]
+    PLAYER1.board = BOARD1
     erb :index
 	end
 
   post '/setup' do
     @player2 = params[:player2]
     GAME.add_player(PLAYER2)
-    GAME.player2.name = params[:player2]    
+    GAME.player2.name = params[:player2]
+    PLAYER2.board = BOARD2
     erb :setup
   end
 
-  get '/setup' do
-    puts GAME.inspect
+  get '/game/player1' do
+    @board = PLAYER1.board.grid
+    erb :game
   end
 
 	run! if app_file == $0
